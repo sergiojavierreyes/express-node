536c1ea91319646e8e8435a16be6f1e6c7028107
Sergio Reyes <sergio.javier.reyes@me.com>
2016-10-17 12:39:07 +0200

    app

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require( 'express')
 const app = express()
 
-app.get('/', (request, response) => {
-	response.send('Hello World')
+app.get('/', (request, response) => 
+	response.status(418) 
+	response.send('Hello World!!!!!!')
 })
 
 app.listen(2000, () => {
 	console.log(' I am Listening 	')
 })
+
+
+
+
+
+
+
+
+
